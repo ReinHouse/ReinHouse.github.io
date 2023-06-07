1cc7dcf4981be9ccbd1f9e613b3c0a96f71e49c4
Bernny V <bernny.velasquez@vikings.berry.edu>
2023-06-07 00:00:05 -0400

    reset page to top

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import React from 'react';
 import Header from "./Header";
 import Main from "./Main"
+import ScrollToTop from './scrollToTop';
+
 const App = () => (
+
     <div className="App">
+      <ScrollToTop/>
       <Header/>
       <Main/> 
     </div>
