269dabee258c172cbf92a1098ac3984bb2d5c7e5
Bernny V <bernny.velasquez@vikings.berry.edu>
2023-06-08 20:13:44 -0400

    timed intervals

--- src/Testomonials.js
+++ src/Testomonials.js
@@ -11,92 +11,92 @@ function Testomonials() {
 			<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
 				
 				<div class="carousel-inner">
-					<div class=" carousel-item active">
+					<div class=" carousel-item active" data-bs-interval="20000" >
 						
 						<p class="testimonial">It’s always a pleasure working with Nate at ReinHouse Design! He is on time, very professional, gave a great presentation and most importantly was kind to the residents of the complex that we were working on. He notified me at every step of the way, was prompt and had a ton of information to present when I needed it most. He finished our decks way ahead of schedule. Thanks Nate! </p>
 						<p class="overview"><b>-Chris S.</b></p>
 					</div>
-					<div class=" carousel-item">
+					<div class=" carousel-item" data-bs-interval="30000">
 						
 						<p class="testimonial">We loved our experience with ReinHouse Design, from our first meeting with Nate to the finished product. Nate came to our house a couple of times and walked us through what to expect. He also showed us what to look for when it comes to quality work and avoiding scammers. When the day came to install the new roof, the crew arrived on time, and worked quickly and efficiently. When they were done, they left the area extremely clean. We love the look of the new roof and are excited about the quality. You can tell that ReinHouse Design takes their business seriously and is a company that strives to make this experience as painless as possible for their customers. I would absolutely recommend them to anyone who is looking for an honest company who is also reliable and does the work they promise with excellence and integrity.</p>
 						<p class="overview"><b>-Susan P.</b></p>
 					</div>
-					<div class=" carousel-item">
+					<div class=" carousel-item" data-bs-interval="20000">
 					
 						<p class="testimonial">Great working with ReinHouse Design. So glad I found them. They did a great job of the repairs, were on time, on budget, and with high quality work. Nate was great with communication updates and responding to queries. He was on-site, had tons of knowledge and was extremely helpful. I would use ReinHouse Design again</p>
 						<p class="overview"><b> -Emily S.</b></p>
 					</div>
-					<div class=" carousel-item ">
+					<div class=" carousel-item " data-bs-interval="20000">
 						
 						<p class="testimonial">Nate with ReinHouse Design should be your first person to contact for any roofing needs. He's knowledgeable, friendly, and fair. He was doing work at my neighbor's house, and I asked if he could look at a spot near my chimney. He quickly diagnosed the situation and repaired it. I've had a hard time getting any other roofers out here to even provide an estimate so I'm super thankful for Nate and ReinHouse Design. </p>
 						<p class="overview"><b>-Steve W.</b></p>
 					</div>
-					<div class=" carousel-item">
+					<div class=" carousel-item" data-bs-interval="20000">
 						
 						<p class="testimonial">In an age where customer service seems to be sadly going by the wayside, I’m pleased to say that is not the case with ReinHouse Design! The people at RHD actually care and it shows through their professionalism, responsiveness, and quality of work. I had one minor concern come up a while after they replaced my whole roof, and they went above and beyond to come out and make sure it was addressed promptly. I very highly recommend them! </p>
 						<p class="overview"><b> -Ben L.</b></p>
 					</div>
-					<div class=" carousel-item">
+					<div class=" carousel-item" data-bs-interval="20000">
 					
 						<p class="testimonial">We sustained hail damage to our roof and had ReinHouse Design do the Repair and Replacement of our roof. Nate and his crew were very punctual and quality oriented in their work. RHD was very professional through the process of replacing our roof and we would use them again as well as refer them to other people we know. Thanks!</p>
 						<p class="overview"><b> -Gary B.</b></p>
 					</div>
-					<div class=" carousel-item">
+					<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">ReinHouse Design built a fence for us around our yard. We had some hail damage and some rotten panels. Nate worked with our insurance company from start to finish as he assessed the initial damage, negotiated with the insurance company, and then installed a beautiful new cedar fence. The entire process was easy, and we felt Nate really was an advocate for us. Thanks for the great experience. We have recommended Nate and RHD to anyone we hear needing help with their home.</p>
 					<p class="overview"><b>-Jimmy P.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">5 Star Rating for ReinHouse Design! My family and I were thoroughly impressed by their hard work, time management, efficiency and communication. From start to finish, Nate was transparent about the work he would perform and made sure that we were completely involved in everything going on. They worked well with our insurance and provided a beautiful, finished product with renewed siding and new windows for our home; we couldn’t be happier with the work this company did! </p>
 					<p class="overview"><b> -Tom M.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">RHD helped us with our insurance claim for a new roof. Nate and his team were timely, professional, and friendly. They helped us work with our insurance company and kept us informed of what was going on and the status of our claim. When it came time to put on the roof Abel, and his crew did a great job installing our new roof and cleaned up the area wonderfully. Working with insurance companies can be very difficult but it was easy, and we are so grateful for the experience we had with ReinHouse Design.</p>
 					<p class="overview"><b>-Tobi S.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">ReinHouse Design was recommended to us by a friend to assist with our new roof from hail damage. Nate and his team were very informed and helpful. They also walked us through the whole process and were with us every step of the way. We can’t recommend RHD enough.</p>
 					<p class="overview"><b>-Charlie M.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">We had major hail damage to our cedar shake roof in July 2019, but our insurance company denied full replacement costs. We contacted several roofing contractors; none of them were willing to take on our insurance company. Finally, we found Nate from ReinHouse Design who did a very detailed estimate, was knowledgeable and was an advocate for through the appeals process. The result was we got a wonderful new roof! We would highly recommend ReinHouse Design. </p>
 					<p class="overview"><b>– Susie T.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">ReinHouse Design did such a fantastic job! The team that came to replace my roof were on time and finished quickly. I also had gutter installation, and soffit and fascia repair. All the repairs and installations were seamless, and they made sure to keep me up to date with the schedule changes due to poor weather. They also did great with the cleanup, not a single nail was left behind. I’d 100% recommend them to anyone!</p>
 					<p class="overview"><b>– Dani B.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">ReinHouse Design replaced our roof and installed new gutters for our home due to some hail damage. Although the roof replacement process itself is very messy, the crew did a fantastic job cleaning up the job site once the roof was completed.</p>
 					<p class="overview"><b>– John G.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">ReinHouse Design installed my new roof last fall and I couldn't be happier with the job they did. Very efficient and very professional. Additionally, I recently had some minor wind damage from a recent storm which had caused a piece of fascia wrap and soffit to come off. I called them up to see if they might be able to reattach the pieces and Nate came out and took care of it quickly. I was impressed with the responsiveness. I highly recommend RHD!</p>
 					<p class="overview"><b>-Aaron K.</b></p>
 				</div>
-				<div class=" carousel-item " data-bs-interval="10000">
+				<div class=" carousel-item " data-bs-interval="20000">
 					
 					<p class="testimonial">We had some damage in our neighborhood from a recent hailstorm and we didn’t know where to start. We contacted ReinHouse Design for help. Nate came out, spent a lot of time explaining to us the process, looked at our roof, our property and assessed the damage. He helped us call-in our claim to our insurance company. He then met with the adjuster on our behalf, and we have our roof scheduled for the next couple weeks. We are thrilled with the service we received from the whole team at RHD. We can’t wait for our new roof.</p>
 					<p class="overview"><b>– Tony F.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">Everyone at ReinHouse Design was very professional and helpful during my complete gutter replacement. Nate did a great job pricing the job and explaining everything without being pushy. Chris and his whole team went to great lengths to ensure the gutters worked properly and that I was happy with the installation. This is one of the few companies that I can truly recommend working with. They will definitely get my business when I’m ready to get a new roof. Thank you, RHD, for your help!</p>
 					<p class="overview"><b> – Sarah M.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">Nate and the team at RHD have done many projects for me through the years at multiple properties. I wouldn’t even think of hiring anyone else to do home projects for our family. Nate is easy to work with and knows this business inside and out. We look forward to our new patio this summer.</p>
 					<p class="overview"><b>– Cassie J.</b></p>
 				</div>
-				<div class=" carousel-item">
+				<div class=" carousel-item" data-bs-interval="20000">
 					
 					<p class="testimonial">ReinHouse Design is a great company to work with! We've been referring our clients to them since their business began. They always provide prompt, efficient and professional service. They do a great job explaining all aspects of a claim so our clients understand what needs to be done. We always refer ReinHouse Design because they are always honest with our clients and they take the extra steps to make sure the client is taken care of. We truly enjoy working with this company!</p>
 					<p class="overview"><b>-Dave G.</b></p>
